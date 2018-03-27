84b6ddab3df7d382252961b32475d5ed820119b4
Abbas Qamar <abbas.qamar@conure.com>
2018-03-27 13:11:39 +0500

    DbContract Class added which contain the database configuration

---
+++ app/Util_Rpt/DbContract.java
@@ -0,0 +1,15 @@
+package Util_Rpt;
+
+/**
+ * Created by Abbas Qamar on 3/27/2018.
+ */
+public class DbContract {
+
+    public static final String HOST = "jdbc:postgresql://localhost:5432/";
+
+    public static final String DB_NAME = "conurets_automation";
+
+    public static final String USERNAME = "postgres";
+
+    public static final String PASSWORD = "";
+}
